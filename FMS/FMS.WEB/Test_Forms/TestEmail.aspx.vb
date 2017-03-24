e52335f7491d37a0fec6a3b37cfd171a56ef13cc
aman <aman@bb12bcb9-4cdd-491f-8423-897dffeb0973>
2017-03-24 13:16:27 +0000

    Test Page for mailing

---
+++ FMS/FMS.WEB/Test_Forms/TestEmail.aspx.vb
@@ -0,0 +1,13 @@
+﻿
+Public Class TestEmail
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+    Protected Sub btnSendNow_Click(sender As Object, e As EventArgs) Handles btnSendNow.Click
+
+
+        
+    End Sub
+End Class
