361c9f71c4a29814f0c1f5bb6447d23420f762d3
aman <aman@bb12bcb9-4cdd-491f-8423-897dffeb0973>
2017-03-31 11:23:53 +0000

    update email reporting service changes

--- FMS.ReportService/Program.cs
+++ FMS.ReportService/Program.cs
@@ -5,27 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using Topshelf;
 using System.Timers;
+using NLog;
+using NLog.Fluent;
+
 
 namespace FMS.ReportService
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public static void Main()
-        {
-            HostFactory.Run(x =>
+        {   
+            try
             {
-                x.Service<IService>(s =>
+                HostFactory.Run(x =>
                 {
-                    s.ConstructUsing(name => new EmailService());
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
-                x.RunAsLocalSystem();
+                    x.Service<IService>(s =>
+                    {
+                        s.ConstructUsing(name => new EmailService());
+                        s.WhenStarted(tc => tc.Start());
+                        s.WhenStopped(tc => tc.Stop());
+                    });
+                    x.RunAsLocalSystem();
 
-                x.SetDescription("FMSReportService for Email Scheduler");
-                x.SetDisplayName("FMSReportService");
-                x.SetServiceName("FMSReportService");
-            });
+                    x.SetDescription("FMSReportService for Email Scheduler");
+                    x.SetDisplayName("FMSReportService");
+                    x.SetServiceName("FMSReportService");
+                });
+            }
+            catch (Exception ex) 
+            {
+                logger.Error("Error with query {0} {1} {2}", "Main Constructor exception", ex.Message, ex.StackTrace);
+            }
         }
     }
 }
