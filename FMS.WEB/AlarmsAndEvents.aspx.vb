64f5a622b2478c07f9ed59695fae727406704000
jonathan <jonathan@bb12bcb9-4cdd-491f-8423-897dffeb0973>
2017-07-21 11:23:49 +0000

    Commit changes for UW-195

--- FMS.WEB/AlarmsAndEvents.aspx.vb
+++ FMS.WEB/AlarmsAndEvents.aspx.vb
@@ -4,7 +4,7 @@ Public Class AlarmsAndEvents
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        dgvEventConfiguration.StartEdit(2)
+        'dgvEventConfiguration.StartEdit(2)
     End Sub
-
+    
 End Class
