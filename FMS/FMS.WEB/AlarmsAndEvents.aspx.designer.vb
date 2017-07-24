cb540179a8d71ec780e831fcbba921d5ed04a476
jonathan <jonathan@bb12bcb9-4cdd-491f-8423-897dffeb0973>
2017-07-24 13:42:19 +0000

    fix for UW201

--- FMS/FMS.WEB/AlarmsAndEvents.aspx.designer.vb
+++ FMS/FMS.WEB/AlarmsAndEvents.aspx.designer.vb
@@ -14,6 +14,15 @@ Option Explicit On
 Partial Public Class AlarmsAndEvents
 
     '''<summary>
+    '''TextBox1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
     '''ASPxPageControl1 control.
     '''</summary>
     '''<remarks>
--- FMS/FMS.WEB/AlarmsAndEvents.aspx.vb
+++ FMS/FMS.WEB/AlarmsAndEvents.aspx.vb
@@ -7,4 +7,45 @@ Public Class AlarmsAndEvents
         'dgvEventConfiguration.StartEdit(2)
     End Sub
     
+    Protected Sub ASPxGridView1_RowValidating(sender As Object, e As Data.ASPxDataValidationEventArgs)
+        For Each column As GridViewColumn In ASPxGridView1.Columns
+            Dim dataColumn As GridViewDataColumn = TryCast(column, GridViewDataColumn)
+            If dataColumn Is Nothing Then
+                Continue For
+            End If
+            If (dataColumn.FieldName.Equals("VehicleID") And e.NewValues(dataColumn.FieldName) Is Nothing) Or _
+                          (dataColumn.FieldName.Equals("Metric") And e.NewValues(dataColumn.FieldName) Is Nothing) Or _
+                          (dataColumn.FieldName.Equals("QueryType") And e.NewValues(dataColumn.FieldName) Is Nothing) Or _
+                          (dataColumn.FieldName.Equals("TextValue") And e.NewValues(dataColumn.FieldName) Is Nothing) Then
+                e.RowError = dataColumn.FieldName & " field Value can't be null."
+                Exit For
+            End If
+        Next column
+        If e.NewValues("QueryType") IsNot Nothing AndAlso (Not e.NewValues("QueryType").ToString().Equals("=") Or Not e.NewValues("QueryType").ToString().Equals("Like")) Then
+            Dim intVal As Integer = 0
+            Dim dblVal As Double = 0
+            If Not e.NewValues("TextValue") Is Nothing Then
+                If Not Integer.TryParse(e.NewValues("TextValue").ToString(), intVal) Then
+                    AddError(e.Errors, ASPxGridView1.Columns("TextValue"), "Text value should not be string.")
+                End If
+                If Not Double.TryParse(e.NewValues("TextValue").ToString(), dblVal) Then
+                    AddError(e.Errors, ASPxGridView1.Columns("TextValue"), "Text value should not be string.")
+                End If
+            End If
+
+        End If
+
+    End Sub
+    Private Sub AddError(ByVal errors As Dictionary(Of GridViewColumn, String), ByVal column As GridViewColumn, ByVal errorText As String)
+        If errors.ContainsKey(column) Then
+            Return
+        End If
+        errors(column) = errorText
+    End Sub
+ 
+    Protected Sub ASPxGridView1_StartRowEditing(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxStartRowEditingEventArgs)
+        If (Not ASPxGridView1.IsNewRowEditing) Then
+            ASPxGridView1.DoRowValidation()
+        End If
+    End Sub
 End Class
