9f030d0524b39c9d7b95facc42743f5c7edfe808
jonathan <jonathan@bb12bcb9-4cdd-491f-8423-897dffeb0973>
2017-11-09 04:48:02 +0000

    commit mainmenu.master uncommitted files

---
+++ FMS/FMS.WEB/MainMenu.master.vb
@@ -0,0 +1,16 @@
+Imports FMS.Business.DataObjects.FeatureListConstants
+Imports FMS.Business
+
+Public Class MainMenuMaster
+    Inherits System.Web.UI.MasterPage
+
+
+    Private Sub Page_Init(sender As Object, e As EventArgs) Handles Me.Init
+
+
+
+    End Sub
+
+
+
+End Class
